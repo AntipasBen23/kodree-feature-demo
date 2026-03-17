fb871371ddcc769f987f143829e1e6177b0035c2
AntipasBen23 <benantipas@gmail.com>
2026-03-17 11:44:02 +0100

    fixed sidebar issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,17 +30,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex bg-gray-50`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-gray-900`}
       >
         <SimulationProvider>
-          <Sidebar />
-          <div className="flex-1 flex flex-col min-h-screen isolate overflow-hidden">
-            <Header />
-            <main className="flex-1 overflow-auto bg-gray-50/50 relative">
-              {children}
-            </main>
+          <div className="flex h-screen w-screen overflow-hidden">
+            <Sidebar />
+            <div className="flex-1 flex flex-col h-full isolate overflow-hidden bg-gray-50">
+              <Header />
+              <main className="flex-1 overflow-y-auto relative p-0 m-0 w-full h-full">
+                {children}
+              </main>
+            </div>
+            <AIPanel />
           </div>
-          <AIPanel />
         </SimulationProvider>
       </body>
     </html>
